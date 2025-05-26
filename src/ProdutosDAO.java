0f44d7759a15a2329325cd72fd11ac5809136e63
onmikron <82680477+onmikronDev@users.noreply.github.com>
2025-05-26 05:32:11 -0300

    Tela Listar produtos vendidos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -100,5 +100,35 @@ public class ProdutosDAO {
             }
         }
     }
+
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos WHERE status = 'vendido'";
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + e.getMessage());
+        } finally {
+            try {
+                conn.close();
+                prep.close();
+                resultset.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
+        return listagem;
+    }
 }
 
