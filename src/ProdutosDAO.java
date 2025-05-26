c0739087fd0e48ec1d81d5505177b3cb62dbe2fa
onmikron <82680477+onmikronDev@users.noreply.github.com>
2025-05-26 04:56:50 -0300

    função listar produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -49,7 +49,32 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos";
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+        } finally {
+            try {
+                conn.close();
+                prep.close();
+                resultset.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
         return listagem;
     }
     
